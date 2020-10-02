eda91aafaf12aa2b82602ca589b187f0a3a14357
Cesar Sanchez <c_sanchez21@hotmail.com>
2020-10-01 20:44:50 -0700

    First Commit

---
+++ ManasaStones/ManasaStones/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ManasaStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stones = 3;
+            int a = 1;
+            int b = 2;
+            int val = 0;
+            List<int> values = new List<int>();
+            for(int i = 0; i < stones; i++)
+            {
+                for(int j = 0; j < stones-i; j++)
+                {
+                    val = +b;
+                }
+                values.Add(val);
+                val = (i + 1) * a;
+            }
+            values.Sort();
+
+            foreach (int v in values)
+                Console.Write(v + " ");
+            
+        }
+    }
+}
